00ad73313601b6bd2b2631a132a9b488ab2ae4a1
강동우 <zaxscdvf22@naver.com>
2023-05-19 06:55:03 +0900

    feat: 프론트 새로 시작, 기본 주소, 사이드메뉴 설정, 에셋 추가

---
+++ final_pjt_front/src/router/index.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ArticleView from '@/views/ArticleView'
+import CreateView from '@/views/CreateView'
+import DetailView from '@/views/DetailView'
+import SignUpView from '@/views/SignUpView'
+import LogInView from '@/views/LogInView'
+import UserProfileView from '@/views/UserProfileView'
+import DetailedSearchView from '@/views/DetailedSearchView'
+import BoxOfficeView from '@/views/BoxOfficeView'
+import FeaturedMovieView from '@/views/FeaturedMovieView'
+import CommunityView from '@/views/CommunityView'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'ArticleView',
+    component: ArticleView
+  },
+ 
+  {
+    path: '/create',
+    name: 'CreateView',
+    component: CreateView
+  },
+
+  {
+    path: '/signup',
+    name: 'SignUpView',
+    component: SignUpView
+  },
+
+  {
+    path: '/login',
+    name: 'LogInView',
+    component: LogInView
+  },
+
+  {
+    path: '/:id',
+    name: 'DetailView',
+    component: DetailView,
+  },
+
+  {
+    path: '/:id/profile',
+    name: 'UserProfileView',
+    component: UserProfileView,
+  },
+
+  {
+    path: '/detailsearch',
+    name: 'DetailedSearchView',
+    component: DetailedSearchView,
+  },
+
+  {
+    path: '/boxoffice',
+    name: 'BoxOfficeView',
+    component: BoxOfficeView,
+  },
+
+  {
+    path: '/featured',
+    name: 'FeaturedMovieView',
+    component: FeaturedMovieView,
+  },
+
+  {
+    path: '/community',
+    name: 'CommunityView',
+    component: CommunityView,
+  },
+
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
